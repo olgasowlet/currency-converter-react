1453fecaee964dd551aec92d6417de030d95e981
Olga Sowińska <olga.sowinska00@gmail.com>
2020-09-29 18:35:27 +0200

    Updates

---
+++ precache-manifest.3cae346f1267e1f17f47a90cb8b3ef08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee68e5a2045598f4a7e9d4eb3a80e4b",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "8940c93d67a16887bd7c",
+    "url": "/currency-converter-react/static/css/main.5985911e.chunk.css"
+  },
+  {
+    "revision": "a7587fbb925fdf32f32e",
+    "url": "/currency-converter-react/static/js/2.1641e76d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.1641e76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8940c93d67a16887bd7c",
+    "url": "/currency-converter-react/static/js/main.a48c5e03.chunk.js"
+  },
+  {
+    "revision": "40f483ef5d2d5cd6c28c",
+    "url": "/currency-converter-react/static/js/runtime-main.c593858c.js"
+  },
+  {
+    "revision": "8907ff2a36452aff59d14c037872718d",
+    "url": "/currency-converter-react/static/media/background.8907ff2a.jpg"
+  }
+]);
