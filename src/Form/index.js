8337a1a1eec903064933f64500d012b86d4c1227
Olga Sowińska <olga.sowinska00@gmail.com>
2020-10-01 11:30:18 +0200

    input value catched by useSate

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,32 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Result from "./Result";
 
-const Form = (props) => (
-    <form className="conventer">
-        <label className="conventer__label">
-            Kwota: <input className="conventer__input" type="number" min="0" required />
-        </label>
-        <label className="conventer__label">
-            Z:
-        <select className="conventer__select" name="currency" required>
-                <option value="PLN">PLN</option>
-                <option value="EUR">EUR</option>
-                <option value="USD">USD</option>
-                <option value="CZK">CZK</option>
-            </select></label>
-        <label className="conventer__label">
-            Na:
-        <select className="conventer__select" name="currency" required>
-                <option value="PLN">PLN</option>
-                <option value="EUR">EUR</option>
-                <option value="USD">USD</option>
-                <option value="CZK">CZK</option>
-            </select>
-        </label>
-        <button className="conventer__button">Przelicz</button>
-        <Result />
-    </form>
-);
+
+const Form = (props) => {
+
+    const [amount, setAmount] = useState(0);
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Kwota: ${amount}`);
+    };
+
+    const onInputChange = ({ target }) => setAmount(target.value);
+
+
+    return (
+        <form className="conventer" onSubmit={onFormSubmit}>
+            <label className="conventer__label">
+                Kwota: <input className="conventer__input" value={amount} onChange={onInputChange} type="number" min="0" required />
+            </label>
+            <label className="conventer__label">
+                Z:
+            <select className="conventer__select" name="currency" required>
+                    <option value="PLN">PLN</option>
+                    <option value="EUR">EUR</option>
+                    <option value="USD">USD</option>
+                    <option value="CZK">CZK</option>
+                </select></label>
+            <label className="conventer__label">
+                Na:
+            <select className="conventer__select" name="currency" required>
+                    <option value="PLN">PLN</option>
+                    <option value="EUR">EUR</option>
+                    <option value="USD">USD</option>
+                    <option value="CZK">CZK</option>
+                </select>
+            </label>
+            <button className="conventer__button">Przelicz</button>
+            <Result />
+        </form>
+    );
+};
 
 export default Form;
