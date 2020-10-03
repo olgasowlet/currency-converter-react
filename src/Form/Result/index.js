722c0928f0a987060df7dc9526ed68af233f54c4
Olga Sowińska <olga.sowinska00@gmail.com>
2020-10-03 11:46:01 +0200

    changed styles in Result and add space entity

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 const Result = (props) => (
-    <div className="conventer__result">{props.result.toFixed(2)} {props.toCurrency}</div>
+    <div className="conventer__result">{props.result.toFixed(2)}&nbsp;{props.toCurrency}</div>
 );
 
 
