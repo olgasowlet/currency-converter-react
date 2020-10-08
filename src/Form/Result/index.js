d15b8c9726ec4fe3498b412ae1d0422c93b3762c
Olga Sowińska <olga.sowinska00@gmail.com>
2020-10-08 22:20:24 +0200

    fixed spelling mistake

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 const Result = ({ result }) => (
-    <div className="conventer__result">{!!result ? <> {result.amount.toFixed(2)}&nbsp;{result.toCurrency} </> : " "}</div>
+    <div className="converter__result">{!!result ? <> {result.amount.toFixed(2)}&nbsp;{result.toCurrency} </> : " "}</div>
 );
 
 
--- src/Form/index.js
+++ src/Form/index.js
@@ -41,25 +41,25 @@ const Form = (props) => {
     };
 
     return (
-        <form className="conventer" onSubmit={onFormSubmit}>
-            <label className="conventer__label">
-                Kwota: <input className="conventer__input" value={amount} onChange={onInputChange} type="number" min="0" required />
+        <form className="converter" onSubmit={onFormSubmit}>
+            <label className="converter__label">
+                Kwota: <input className="converter__input" value={amount} onChange={onInputChange} type="number" min="0" required />
             </label>
-            <label className="conventer__label">
+            <label className="converter__label">
                 Z:
-            <select className="conventer__select" value={fromCurrency} onChange={onFirstSelectChange} name="currency" required>
+            <select className="converter__select" value={fromCurrency} onChange={onFirstSelectChange} name="currency" required>
                     {currencies.map(currency => (
                         <option key={currency.value} value={currency.value}>{currency.value}</option>
                     ))}
             </select></label>
-            <label className="conventer__label">
+            <label className="converter__label">
                 Na:
-            <select className="conventer__select" value={toCurrency} onChange={onSecondSelectChange} name="currency" required>
+            <select className="converter__select" value={toCurrency} onChange={onSecondSelectChange} name="currency" required>
                     {currencies.map(currency => (
                         <option key={currency.value} value={currency.value}>{currency.value}</option>
                     ))}
             </select></label>
-            <button className="conventer__button">Przelicz</button>
+            <button className="converter__button">Przelicz</button>
             <Result result={result} />
         </form>
     );
