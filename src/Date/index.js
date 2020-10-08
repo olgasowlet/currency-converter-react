ae14818efeec8d4c8c2b1813da8e3de7f3cc8cb7
Olga Sowińska <olga.sowinska00@gmail.com>
2020-10-08 22:04:48 +0200

    create formatDate function and added media queries

--- src/Date/index.js
+++ src/Date/index.js
@@ -1,22 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 
+const formatDate = (date) => {
+    return date.toLocaleString("pl",
+        {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"
+        });
+};
+
 const PlaceForDate = () => {
-    const [date, setDate] = useState();
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const intervalID = setInterval(() => {
-            setDate(date => new Date().toLocaleString( "pl", {weekday: "long", day:"numeric", month:"long", year:"numeric", hour:"numeric", minute:"numeric", second:"numeric"}));
+            setDate(new Date());
         }, 1000);
 
         return () => {
             clearInterval(intervalID);
         };
-    }, [])
+    }, []);
 
     return (
         <div className="dateContainer">
-            <span>Dzisiaj jest {date}</span>
+            <span>Dzisiaj jest {formatDate(date)}</span>
         </div>
     );
 };
