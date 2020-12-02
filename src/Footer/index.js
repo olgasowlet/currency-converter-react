c95a0c81ed80aeaf7e00236a3af1ca6eb291e2b3
Olga Sowińska <olga.sowinska00@gmail.com>
2020-12-02 17:41:34 +0100

    added date to footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { StyledFooter } from "./style";
+import { useApiData } from "../apiData";
 
-const Footer = () => (
-    <StyledFooter>
-        Pozdrawiam i życzę miłego dnia - Olga ;*
+const Footer = () => {
+    const date = useApiData();
+
+    return <StyledFooter>
+        Dane z {date.date}. Pozdrawiam i życzę miłego dnia - Olga ;*
     </StyledFooter>
-);
+};
 
 export default Footer;
