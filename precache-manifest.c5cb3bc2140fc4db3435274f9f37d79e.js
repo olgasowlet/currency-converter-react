ee48c105179b5f164a3438b0c77b2c8bb2f0b445
Olga Sowińska <olga.sowinska00@gmail.com>
2020-12-03 12:18:39 +0100

    Updates

---
+++ precache-manifest.c5cb3bc2140fc4db3435274f9f37d79e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ec12fd5854e054725ef5cc874ffcc5",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "e08655cec9380e2631a4",
+    "url": "/currency-converter-react/static/js/2.6eec167f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency-converter-react/static/js/2.6eec167f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af9419a1b068c0fce6d",
+    "url": "/currency-converter-react/static/js/main.409ee5fb.chunk.js"
+  },
+  {
+    "revision": "40f483ef5d2d5cd6c28c",
+    "url": "/currency-converter-react/static/js/runtime-main.c593858c.js"
+  },
+  {
+    "revision": "8907ff2a36452aff59d14c037872718d",
+    "url": "/currency-converter-react/static/media/background.8907ff2a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/currency-converter-react/precache-manifest.bb111016c3b963c4fbc87a6b79388383.js"
+  "/currency-converter-react/precache-manifest.c5cb3bc2140fc4db3435274f9f37d79e.js"
 );
 
 self.addEventListener('message', (event) => {
